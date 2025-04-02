4289ac6717efa9309fdc19c6eedc369cc2fe492c
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-02 11:33:22 +0200

    style: enhance ChatInput component with improved styling and layout

--- frontend/src/features/Chat/ChatInput.tsx
+++ frontend/src/features/Chat/ChatInput.tsx
@@ -6,17 +6,17 @@ function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
 
 export default function ChatInput() {
   return (
-    <div>
+    <div className="px-4">
       <form
-        className="mx-8 my-4 flex items-center rounded-3xl bg-teal-900 px-1 py-4 transition-all duration-300 focus-within:-translate-y-1"
+        className="mx-auto my-4 flex max-w-5xl items-center rounded-3xl bg-teal-900 px-1 py-4 shadow-md shadow-teal-900/50 transition-all duration-300 focus-within:-translate-y-1 focus-within:bg-teal-950"
         onSubmit={handleSubmit}
       >
         <textarea
-          className="scrollbar scrollbar-thumb-[#ffdcc6]/50 scrollbar-track-teal-900 flex-1 resize-none overflow-scroll overflow-x-hidden overflow-y-auto p-4 text-teal-50 placeholder:text-base focus:outline-none"
+          className="scrollbar scrollbar-thumb-[#ffdcc6]/50 scrollbar-track-teal-900 focus:scrollbar-track-teal-950 flex-1 resize-none overflow-scroll overflow-x-hidden overflow-y-auto p-4 text-teal-50 placeholder:text-base focus:outline-none"
           rows={1}
           placeholder="Ask me something ..."
         />
-        <button className="mr-4 cursor-pointer rounded-full p-3 text-teal-50 transition-all duration-300 hover:bg-teal-700">
+        <button className="focus-within:scrollbar-track-teal-900 mr-4 cursor-pointer rounded-full p-3 text-teal-50 transition-all duration-300 hover:bg-teal-700 focus:ring-2 focus:outline-none">
           <SendHorizontal size={25} />
         </button>
       </form>
