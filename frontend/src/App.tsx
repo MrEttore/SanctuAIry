136d5b13478b29fa6f64317d393915f6eb5c5726
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-10 17:25:51 +0200

    feat: refactor App component to use react-router for navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import { Navigation } from './features/Navigation/Navigation';
-import { Chat } from './features/Chat/Chat';
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Main } from './pages/Main';
 
 export function App() {
-  return (
-    <div className="font-quicksand flex h-full w-full">
-      <Navigation />
-      <Chat />
-    </div>
-  );
+    return (
+        <div className="font-quicksand flex h-full w-full">
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/chat" element={<Main />} />
+            </Routes>
+        </div>
+    );
 }
