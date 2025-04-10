41749a0d00da27a01bb556fc4847ca9fc7cba634
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-10 17:25:16 +0200

    feat: add furtherStyles prop to Logo component for customizable styling

--- frontend/src/ui/Logo.tsx
+++ frontend/src/ui/Logo.tsx
@@ -1,12 +1,18 @@
 import logo from '../assets/Logo-Teal-50.svg';
 
-export function Logo() {
-  return (
-    <div className="flex cursor-pointer items-center justify-center pr-4 pb-4">
-      <img src={logo} alt="Logo" className="h-40 w-40"></img>
-      <p className="my-4 text-5xl text-teal-50">
-        Sanctu<span className="font-semibold">AI</span>ry
-      </p>
-    </div>
-  );
+type Props = {
+    furtherStyles?: string;
+};
+
+export function Logo({ furtherStyles }: Props) {
+    return (
+        <div
+            className={`flex cursor-pointer items-center justify-center ${furtherStyles}`}
+        >
+            <img src={logo} alt="Logo" className="h-40 w-40"></img>
+            <p className="my-4 text-5xl text-teal-50">
+                Sanctu<span className="font-semibold">AI</span>ry
+            </p>
+        </div>
+    );
 }
