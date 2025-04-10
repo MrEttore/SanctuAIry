e24dbb7533183b523e93934162ce8c02a565a741
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-10 17:29:24 +0200

    feat: refactor userSlice to include setUser reducer for user state management

--- frontend/src/redux/slices/userSlice.ts
+++ frontend/src/redux/slices/userSlice.ts
@@ -1,21 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User } from '../../types/user';
 
 const initialState: User = {
-  username: 'John Doe',
-  email: 'john.doe@gmail.com',
-  profilePicture: 'https://avatar.iran.liara.run/public/4',
+    username: 'John Doe',
+    email: 'john.doe@gmail.com',
+    profilePicture: 'https://avatar.iran.liara.run/public/4',
 };
 
 const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    updateUsername(state, action) {
-      state.username = action.payload;
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser(state, action: PayloadAction<User>) {
+            state.username = action.payload.username;
+            state.email = action.payload.email;
+            state.profilePicture = action.payload.profilePicture;
+        },
     },
-  },
 });
 
-export const { updateUsername } = userSlice.actions;
+export const { setUser } = userSlice.actions;
 export default userSlice.reducer;
