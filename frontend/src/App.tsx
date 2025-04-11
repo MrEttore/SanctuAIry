6d2cf2f76dd455dd0b2a5d3e1345f948032f96bb
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-11 16:09:26 +0200

    fix: update login route to use Auth component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import { Login } from './pages/Login';
+// import { useQuery } from '@tanstack/react-query';
+
+import { Auth } from './pages/Auth';
 import { Main } from './pages/Main';
 
 export function App() {
     return (
         <div className="font-quicksand flex h-full w-full">
             <Routes>
-                <Route path="/login" element={<Login />} />
+                <Route path="/login" element={<Auth />} />
                 <Route path="/chat" element={<Main />} />
             </Routes>
         </div>
