52c80c153f47a939ca065fa7978a8bc180f75021
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-11 11:05:18 +0200

    feat: add UserSummary component to display user information

--- frontend/src/features/User/UserSummary.tsx
+++ frontend/src/features/User/UserSummary.tsx
@@ -1,20 +1,14 @@
-import { MoreVertical } from 'lucide-react';
 import { useSelector } from 'react-redux';
+import { MoreVertical } from 'lucide-react';
 import { RootState } from '../../redux/store';
+import { ProfilePicture } from '../../ui';
 
-export function UserSection() {
-    const { username, email, profilePicture } = useSelector(
-        (state: RootState) => state.user,
-    );
+export function UserSummary() {
+    const { username, email } = useSelector((state: RootState) => state.user);
 
     return (
-        <div className="flex border-t-3 border-[#ffdcc6] p-4">
-            <img
-                className="mr-4 h-20 w-20 cursor-pointer rounded-full border-2 border-transparent transition-all duration-200 hover:border-2 hover:border-teal-50"
-                src={profilePicture}
-                alt=""
-            />
-
+        <div className="flex border-t-3 border-teal-50 p-4">
+            <ProfilePicture />
             <div className="flex flex-1 items-center justify-between text-teal-50">
                 <div className="flex flex-col gap-2">
                     <span className="text-2xl font-bold uppercase">
