fbf0a9af2dfb4f1266a074dce7a0ab80d156b81d
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-11 11:03:42 +0200

    fix: replace UserSection with UserSummary in Navigation component

--- frontend/src/features/Navigation/Navigation.tsx
+++ frontend/src/features/Navigation/Navigation.tsx
@@ -1,13 +1,13 @@
 import { Logo } from '../../ui';
-import { UserSection } from './UserSection';
 import { NavigationMenu } from './NavigationMenu';
+import { UserSummary } from '../User/UserSummary';
 
 export function Navigation() {
     return (
         <aside className="flex shrink-0 flex-col bg-teal-950">
             <Logo furtherStyles="pr-4" />
             <NavigationMenu />
-            <UserSection />
+            <UserSummary />
         </aside>
     );
 }
