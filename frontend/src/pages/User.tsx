1f1f13982817d85d6e19c9d4acc8c7321c6df649
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-21 18:54:09 +0200

    added todo

--- frontend/src/pages/User.tsx
+++ frontend/src/pages/User.tsx
@@ -1,6 +1,7 @@
 export function User() {
     return (
         <main className="flex w-full flex-col bg-teal-50">
+            {/* TODO: Add load, delete models for the user session. */}
             <div>Account Settings</div>
         </main>
     );
