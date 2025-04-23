4b86b402c5a2babbda73a24564c3889bff431811
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-23 10:19:36 +0200

    added link prop to Logo

--- frontend/src/ui/Logo.tsx
+++ frontend/src/ui/Logo.tsx
@@ -1,18 +1,21 @@
+import { Link } from 'react-router-dom';
 import logo from '../assets/Logo-Teal-50.svg';
 
 type Props = {
     furtherStyles?: string;
+    linkTo?: string;
 };
 
-export function Logo({ furtherStyles }: Props) {
+export function Logo({ furtherStyles, linkTo }: Props) {
     return (
-        <div
-            className={`flex cursor-pointer items-center justify-center ${furtherStyles}`}
+        <Link
+            to={linkTo ? linkTo : ''}
+            className={`flex items-center justify-center ${furtherStyles} ${!linkTo ? 'cursor-default' : ''}`}
         >
-            <img src={logo} alt="Logo" className="h-40 w-40"></img>
+            <img src={logo} alt="Logo" className="h-40 w-40" />
             <p className="my-4 text-5xl text-teal-50">
                 Sanctu<span className="font-semibold">AI</span>ry
             </p>
-        </div>
+        </Link>
     );
 }
