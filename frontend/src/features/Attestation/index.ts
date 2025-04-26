e8e857b6ab939e93ac18c87290fb4179a1856490
Ettore Marangon <70102376+MrEttore@users.noreply.github.com>
2025-04-26 14:36:23 +0200

    Delete frontend/src/features/Attestation directory

--- frontend/src/features/Attestation/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './AttestationHeader';
-export * from './AttestationSummary';
-export * from './AttestationTimeline';
-export * from './AttestationEvidence';
-export * from './ImageVerification';
-export * from './IssueChallengeForm';
