08676078ab5d24b946d294b26ca29a280a7fbffd
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-26 14:28:46 +0200

    added todo

--- frontend/src/pages/Overview.tsx
+++ frontend/src/pages/Overview.tsx
@@ -1,4 +1,11 @@
 export function Overview() {
+    /*
+     * TODO: On the overview page the user should get informed about the "confidential claims" made by the * service providers. These claims are then verified with the transparent attestation process.
+     *
+     * Add "Verify Claims" button to start attestation.
+     *
+     */
+
     return (
         <div className="flex flex-1 flex-col items-center justify-center space-y-2 bg-gradient-to-r from-teal-950 from-2% to-slate-800 to-95%">
             <p className="text-5xl font-medium text-teal-50">Overview Page</p>
