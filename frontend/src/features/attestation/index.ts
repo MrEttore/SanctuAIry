4366a910dc526272212cdf88fcac7c02f4c8db4f
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-26 14:30:12 +0200

    renamed folder

---
+++ frontend/src/features/attestation/index.ts
@@ -0,0 +1,6 @@
+export * from './AttestationHeader';
+export * from './AttestationSummary';
+export * from './AttestationTimeline';
+export * from './AttestationEvidence';
+export * from './ImageVerification';
+export * from './IssueChallengeForm';
