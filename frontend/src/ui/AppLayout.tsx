2a14dae11996df1ae09774fcd9ccc581c8e4c13b
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-05-28 17:54:23 +0200

    fix: add styling to AppLayout for better visual separation

--- frontend/src/ui/AppLayout.tsx
+++ frontend/src/ui/AppLayout.tsx
@@ -4,7 +4,7 @@ import { Navigation } from '../features/navigation';
 
 export function AppLayout() {
     return (
-        <div className="flex w-full">
+        <div className="flex w-full divide-x-2 divide-teal-800/20">
             <Navigation />
             <Outlet />
         </div>
