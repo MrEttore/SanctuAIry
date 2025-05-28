6dcc551b99ab5a494fe1f6c040f8b8a448f99b12
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-05-28 17:06:42 +0200

    refactor: clean up imports and remove commented code in AttestationPage

--- frontend/src/features/attestation/AttestationPage.tsx
+++ frontend/src/features/attestation/AttestationPage.tsx
@@ -1,18 +1,14 @@
-import {
-    // AttestationEvidence,
-    // AttestationHeader,
-    AttestationSummary,
-    AttestationTimeline,
-} from '.';
+import { AttestationSummary } from './components/Summary';
+import { AttestationTimeline } from './components/Timeline';
 
 export function AttestationPage() {
     return (
         <main className="flex flex-1 flex-col space-y-3 bg-gradient-to-r from-teal-950 from-2% to-slate-800 to-95%">
-            {/* <AttestationHeader /> */}
             <AttestationSummary />
+            <AttestationTimeline />
+
             {/* TODO: Move Evidence in modal window? */}
             {/* <AttestationEvidence /> */}
-            <AttestationTimeline />
         </main>
     );
 }
