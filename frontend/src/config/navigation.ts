f09209039ca5a665948775748b0141c6d6b7ad38
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-05-28 17:50:57 +0200

    feat: update navigation item name for attestation to "Verify Privacy & Security"

--- frontend/src/config/navigation.ts
+++ frontend/src/config/navigation.ts
@@ -4,6 +4,10 @@ import { NavigationItem } from '../types/navigation';
 
 export const navigationItems: NavigationItem[] = [
     { icon: MessageCircle, name: 'Chat', to: '/app/chat' },
-    { icon: SearchCheck, name: 'Attestation', to: '/app/attestation' },
     { icon: Settings, name: 'Account Settings', to: '/app/user' },
+    {
+        icon: SearchCheck,
+        name: 'Verify Privacy & Security',
+        to: '/app/attestation',
+    },
 ];
