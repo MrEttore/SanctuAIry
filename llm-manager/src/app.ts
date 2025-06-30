f24e6c7bf8f86cf5b5bb53fe8f6c51ebb4c62992
MrEttore <ettoremarangon@gmail.com>
2025-06-30 13:33:29 +0200

    fix: move frontend URL initialization and logging to the top of the app setup

--- llm-manager/src/app.ts
+++ llm-manager/src/app.ts
@@ -8,19 +8,23 @@ import { globalErrorHandler } from './middlewares/globalErrorHandler.js';
 
 export const app = express();
 
+const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+console.info(`Frontend URL: ${frontendUrl}`);
+
 // Middlewares
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 if (process.env.NODE_ENV === 'production') app.use(morgan('combined'));
-const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
-console.info(`Frontend URL: ${frontendUrl}`);
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: [frontendUrl, 'http://localhost', 'https://sanctuairy.netlify.app'],
-  }),
-);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(
+    cors({
+      origin: [frontendUrl, 'http://localhost', 'https://sanctuairy.netlify.app'],
+    }),
+  );
+}
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Llm manager is running');
